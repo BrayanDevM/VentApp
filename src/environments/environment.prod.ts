249dd991331f64330cf5a0f04aa71cebece95447
Brayan Dev <bdevia95@gmail.com>
2021-02-24 00:14:46 -0500

    Fix tema login, Facebook login, redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC3j9A8hCtvK9HZqFYCzAYR5ZF1ib7Yim8',
+    authDomain: 'ventapp-6d0c6.firebaseapp.com',
+    projectId: 'ventapp-6d0c6',
+    storageBucket: 'ventapp-6d0c6.appspot.com',
+    messagingSenderId: '104730920243',
+    appId: '1:104730920243:web:8364bec8d1d3d4907d2a46',
+    measurementId: 'G-28YM2SN2RM',
+  },
 };
